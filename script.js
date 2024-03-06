2ae040c021f0410de566bc73dcbda5cb05a3ec65
Sachin Kumar <123574186+Sachin6096@users.noreply.github.com>
2024-03-06 15:33:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
+let first = document.querySelector(".fname")
+let last = document.querySelector(".last")
+let sub = document.querySelector(."btn")
+
+sub.addEventListener("click",getFormvalue())
 function getFormvalue() {
     //Write your code here
-    let first = document.querySelector(".fname")
-	let last = document.querySelector(".last")
-	let sub = document.querySelector(."btn")
-
-	sub.addEventListener('submit',function(event){
-		event.preventDefault();
-		alert('${first} ${last}');
+    alert('${first.value} ${last.value})
 	})
 }
